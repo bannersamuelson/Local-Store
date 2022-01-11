053ff0456825dbbd9d6046531092e0287069c0ef
Banner Samuelson <banner@Banners-MacBook-Air.local>
2022-01-11 12:42:51 -0500

    store.jsx working properly

--- client/src/components/Stores.jsx
+++ client/src/components/Stores.jsx
@@ -19,7 +19,25 @@ export default function Stores() {
 
   return (
     <div>
-      yo
+      <ul>
+        {stores.map((store) => {
+          return (
+            <li key={store.id}>
+              <div>
+                <h3>{store.fields.storeName}</h3>
+                <img
+                  style={{ width: "300px" }}
+                  src={store.fields.storeImg}
+                  alt={store.fields.storeName}
+                />
+                <h5>{store.fields.storeDescription}</h5>
+                <Link to={`/store/${store.id}`}><button>View More</button> </Link>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   )
 }
+
