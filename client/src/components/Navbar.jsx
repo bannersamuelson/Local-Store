7f969fedb3987e5599c8661a0f415dad94e0e210
Banner Samuelson <banner@Banners-MacBook-Air.local>
2022-01-11 18:25:56 -0500

    hamburger menu works

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,24 +1,48 @@
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
 import '../Navbar.css'
 
+
+
+
+
 export default function Navbar() {
+
+  const [navbarOpen, setNavbarOpen] = useState(false)
+  const [isActive, setActive] = useState(false);
+
+  // const ToggleClass = () => {
+  //   setActive(!isActive);
+  // };
+
+  const handleToggle = () => {
+    setNavbarOpen(prevToggle => !prevToggle)
+    setActive(!isActive);
+  }
+
   return (
-    <div className='navbar'>
-      <div className='container nav-container'>
-        <input class="checkbox" type="checkbox" name="" id="" />
-        <div class="hamburger-lines">
-          <span class="line line1"></span>
-          <span class="line line2"></span>
-          <span class="line line3"></span>
-        </div>
-        <div class="menu-items">
-          <Link to='/'><h1>Home</h1></Link>
-          <ul>
-            <li><Link to='/store'>Stores</Link></li>
-            <li><Link to='/contact'>Contact</Link></li>
+
+    <nav>
+      <div className='navBar'>
+        <div className="container nav-container">
+
+          <button onClick={handleToggle}>
+            <div className={`container ${isActive ? "change" : ""}`}>
+              <div className="bar1"></div>
+              <div className="bar2"></div>
+              <div className="bar3"></div>
+            </div>
+          </button>
+
+
+          <ul className={`menuNav ${navbarOpen ? " showMenu" : ""}`}>
+            <li><Link to='/' onClick={handleToggle}>Home</Link></li>
+            <li><Link to='/store' onClick={handleToggle}>Stores</Link></li>
+            <li><Link to='/contact' onClick={handleToggle}>Contact</Link></li>
           </ul>
+
         </div>
       </div>
-    </div>
+    </nav >
   )
 }
